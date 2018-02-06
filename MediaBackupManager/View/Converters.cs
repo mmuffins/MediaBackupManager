509c99d5851825b4872f97122cde9470423d8264
mmuffins <mail@tiv73.com>
2018-02-06 22:42:35 +0100

    Added value converters

---
+++ MediaBackupManager/View/Converters.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MediaBackupManager.View
+{
+    /// <summary>
+    /// Converts the provided boolean to its inverse value. </summary>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InvertBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    /// <summary>
+    /// Converts Boolean to Visible or either hidden or collapsed and supports inverse conversion. </summary>
+    [ValueConversion(typeof(bool), typeof(System.Windows.Visibility))]
+    public class ParemeteredBooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        public ParemeteredBooleanToVisibilityConverter() { }
+
+        public bool Collapse { get; set; }
+        public bool Reverse { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool bValue = (bool)value;
+
+            if (bValue != Reverse)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                if (Collapse)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+
+            if (visibility == Visibility.Visible)
+                return !Reverse;
+            else
+                return Reverse;
+        }
+    }
+}
