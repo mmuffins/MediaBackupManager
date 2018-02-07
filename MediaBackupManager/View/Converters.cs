58edb8327b9add3730cbd59ea021fe64f88b89ae
mmuffins <mail@tiv73.com>
2018-02-07 19:16:39 +0100

    added byte to human readable converter

--- MediaBackupManager/View/Converters.cs
+++ MediaBackupManager/View/Converters.cs
@@ -68,4 +68,71 @@ namespace MediaBackupManager.View
                 return Reverse;
         }
     }
+
+    /// <summary>
+    /// Converts the given byte number to a human readable format. </summary>
+    [ValueConversion(typeof(long), typeof(string))]
+    public class ByteToHumanReadableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if (targetType != typeof(long) && targetType != typeof(int)  && targetType != typeof(double))
+            //    throw new InvalidOperationException("The target must be a numeric value");
+            long i;
+
+            if(!long.TryParse(value.ToString(), out i))
+                throw new InvalidOperationException("The target must be a numeric value");
+
+            // https://stackoverflow.com/questions/281640
+            // Get absolute value
+            long absolute_i = (i < 0 ? -i : i);
+            // Determine the suffix and readable value
+            string suffix;
+            double readable;
+            if (absolute_i >= 0x1000000000000000) // Exabyte
+            {
+                suffix = "EB";
+                readable = (i >> 50);
+            }
+            else if (absolute_i >= 0x4000000000000) // Petabyte
+            {
+                suffix = "PB";
+                readable = (i >> 40);
+            }
+            else if (absolute_i >= 0x10000000000) // Terabyte
+            {
+                suffix = "TB";
+                readable = (i >> 30);
+            }
+            else if (absolute_i >= 0x40000000) // Gigabyte
+            {
+                suffix = "GB";
+                readable = (i >> 20);
+            }
+            else if (absolute_i >= 0x100000) // Megabyte
+            {
+                suffix = "MB";
+                readable = (i >> 10);
+            }
+            else if (absolute_i >= 0x400) // Kilobyte
+            {
+                suffix = "KB";
+                readable = i;
+            }
+            else
+            {
+                return i.ToString("0 B"); // Byte
+            }
+            // Divide by 1024 to get fractional value
+            readable = (readable / 1024);
+            // Return formatted number with suffix
+            return readable.ToString("0.## ") + suffix;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
