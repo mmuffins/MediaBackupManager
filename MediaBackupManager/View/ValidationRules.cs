95b62a682f519c3a874b62b6aa98288874e00af0
mmuffins <mail@tiv73.com>
2018-02-07 18:24:26 +0100

    added validation rules

---
+++ MediaBackupManager/View/ValidationRules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MediaBackupManager.View
+{
+
+    public class StringNotEmptyRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(!(value is string))
+                return new ValidationResult(false, "Value must be a string.");
+
+            if(string.IsNullOrWhiteSpace(value.ToString()))
+                return new ValidationResult(false, "Value is null or empty.");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+
+}
